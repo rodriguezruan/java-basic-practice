40bd0f2270cccb42081a7d2ea3847a903c20a7fd
RuanRodrigues <ruuansantos32@gmail.com>
2026-02-12 17:47:10 -0300

    class reservd to operators basics added to rep

---
+++ src/Operators.java
@@ -0,0 +1,24 @@
+public class Operators {
+    public static void main(String[] args) {
+        //Aritmetics operators + - / *
+
+        int sum = 1 + 2;
+        int subtraction = 1-5;
+        int division = 10/5;
+        int multiplication = 2 * 6;
+
+        String stack = "JAVA";
+        String name = "Ruan Rodrigues";
+
+        System.out.println("soma: " + sum);
+        System.out.println("subtraction: " + subtraction);
+        System.out.println("Division: " + division);
+        System.out.println("Multiplication: " + multiplication);
+        System.out.println("Nome:" + name + " " + "Stack Trabalhada:" + stack);
+
+        //-- used to decrement
+        //++ used to increment
+        //!  used to negation
+        //=+ actual state and increment
+    }
+}
---
+++ src/VariableTypes.java
@@ -0,0 +1,14 @@
+public class VariableTypes {
+    public static void main(String[] args) {
+        double minimunSalary = 2500.00;
+        int age = 19;
+        final int BIRTH_YEAR = 2006;
+        String nome = "Ruan Rodrigues";
+        short nubmber= 1;
+        long cep = 37545000;
+        double piValue = 3.14;
+        char sexo = 'M';
+        boolean isFree = true;
+
+    }
+}
